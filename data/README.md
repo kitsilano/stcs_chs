e9ef7d4239de54dfebd6873e20e331ad37455006
GiankDiluvi <giankdiluvi@gmail.com>
2021-06-21 14:58:01 -0700

    finalize data wrangling and save data set

--- data/README.md
+++ data/README.md
@@ -7,3 +7,4 @@ Here go all the data files used in the analysis.
 - `CHS_2018_EN.TAB` is the 2018 Canadian Housing Survey in tabular format
 - `CHS user guide_PUMF_EN_final.pdf` is the documentation of the data set
 - `PUMF_CHS2018_E.pdf` is the data dictionary of the data set
+- `chs_clean.csv` is the clean CHS data; output from `../src/data_gen.R`
--- src/data_gen.R
+++ src/data_gen.R
@@ -28,7 +28,7 @@ chs_full <- readr::read_tsv("../data/CHS_2018_EN.tab")
 # subset rows of interest
 chs <- chs_full %>% 
   dplyr::select(PUMFID, PFWEIGHT, PPROV, PGEOGR, PDWLTYPE, PDCT_05, PHHTTINC, EHA_10, PRSPGNDR,
-                PFTHB5YR, PPAC_05, PPAC_30, PPAC_45A, PPAC_45M) %>% 
+                PPAC_05, PPAC_30, PPAC_45A, PPAC_45M) %>% 
   dplyr::rename(id = PUMFID,
                 weight = PFWEIGHT,
                 province = PPROV,
@@ -38,7 +38,6 @@ chs <- chs_full %>%
                 income = PHHTTINC,
                 hardships = EHA_10,
                 gender = PRSPGNDR,
-                ft_buyer = PFTHB5YR,
                 move_date = PPAC_05,
                 pretenure = PPAC_30,
                 forced_move = PPAC_45A,
@@ -51,8 +50,67 @@ chs <- chs_full %>%
                 income != 99999999999,
                 hardships != 9,
                 gender != 9,
-                ft_buyer != 9,
                 move_date != 99,
                 pretenure != 9,
                 forced_move != 9,
-                move_to_buy != 9)
+                move_to_buy != 9) %>% 
+  dplyr::mutate(
+    region = case_when(
+      region == 25 ~ 'Vancouver',
+      region == 26 ~ 'Other CMA',
+      region == 27 ~ 'Non CMA',
+      TRUE ~ 'NA'
+    ),
+    tenure = case_when(
+      tenure == 1 ~ 'owned',
+      tenure == 2 ~ 'rented',
+      TRUE ~ 'NA'
+    ),
+    hardships = case_when(
+      hardships == 1 ~ 'very difficult',
+      hardships == 2 ~ 'difficult',
+      hardships == 3 ~ 'neither',
+      hardships == 4 ~ 'easy',
+      hardships == 5 ~ 'very easy',
+      TRUE ~ 'NA'
+    ),
+    gender = case_when(
+      gender == 1 ~ 'male',
+      gender == 2 ~ 'female',
+      TRUE  ~ 'NA'
+    ),
+    move_date = case_when(
+      move_date == 1 ~ '<1',
+      move_date == 2 ~ '1-2',
+      move_date == 3 ~ '2-3',
+      move_date == 4 ~ '3-4',
+      move_date == 5 ~ '4-5',
+      move_date == 6 ~ '5-10',
+      move_date == 7 ~ '10+',
+      TRUE ~ 'NA'
+    ),
+    pretenure = case_when(
+      pretenure == 1 ~ 'own',
+      pretenure == 2 ~ 'rent',
+      pretenure == 3 ~ 'rent-free',
+      TRUE ~ 'NA'
+    ),
+    forced_move = case_when(
+      forced_move == 1 ~ 'yes',
+      forced_move == 2 ~ 'no',
+      TRUE ~ 'NA'
+    ),
+    move_to_buy = case_when(
+      move_to_buy == 1 ~ 'yes',
+      move_to_buy == 2 ~ 'no',
+      TRUE ~  'NA'
+    ))
+
+
+####################
+####################
+# data saving    ###
+####################
+####################
+
+readr::write_csv(chs, path = '../data/chs_clean.csv')
